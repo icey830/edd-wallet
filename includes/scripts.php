4afeae123d35299f2c6c5f1aa4b1d31c29a25667
Ghost1227 <dgriffiths@section214.com>
2015-09-22 19:17:17 -0500

    Fix whitespace

--- includes/scripts.php
+++ includes/scripts.php
@@ -9,7 +9,7 @@
 
 // Exit if accessed directly
 if( ! defined( 'ABSPATH' ) ) {
-    exit;
+	exit;
 }
 
 
@@ -21,18 +21,18 @@ if( ! defined( 'ABSPATH' ) ) {
  * @return      void
  */
 function edd_wallet_admin_scripts( $hook ) {
-    if( ! apply_filters( 'edd_load_admin_scripts', edd_is_admin_page(), $hook ) ) {
-        return;
-    }
+	if( ! apply_filters( 'edd_load_admin_scripts', edd_is_admin_page(), $hook ) ) {
+		return;
+	}
 
-    // Use minified libraries if SCRIPT_DEBUG is turned off
-    $suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
+	// Use minified libraries if SCRIPT_DEBUG is turned off
+	$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
 
-    wp_enqueue_style( 'edd-wallet', EDD_WALLET_URL . 'assets/css/admin' . $suffix . '.css', EDD_WALLET_VER );
-    wp_enqueue_script( 'edd-wallet', EDD_WALLET_URL . 'assets/js/admin' . $suffix . '.js', EDD_WALLET_VER );
-    wp_localize_script( 'edd-wallet', 'edd_wallet_vars', array(
-        'type_to_search'    => __( 'Type to search levels', 'edd-wallet' )
-    ) );
+	wp_enqueue_style( 'edd-wallet', EDD_WALLET_URL . 'assets/css/admin' . $suffix . '.css', EDD_WALLET_VER );
+	wp_enqueue_script( 'edd-wallet', EDD_WALLET_URL . 'assets/js/admin' . $suffix . '.js', EDD_WALLET_VER );
+	wp_localize_script( 'edd-wallet', 'edd_wallet_vars', array(
+		'type_to_search'    => __( 'Type to search levels', 'edd-wallet' )
+	) );
 }
 add_action( 'admin_enqueue_scripts', 'edd_wallet_admin_scripts', 100 );
 
@@ -44,19 +44,19 @@ add_action( 'admin_enqueue_scripts', 'edd_wallet_admin_scripts', 100 );
  * @return      void
  */
 function edd_wallet_scripts() {
-    // Use minified libraries if SCRIPT_DEBUG is turned off
-    $suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
+	// Use minified libraries if SCRIPT_DEBUG is turned off
+	$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
 
-    wp_enqueue_style( 'edd-wallet', EDD_WALLET_URL . 'assets/css/edd-wallet' . $suffix . '.css', EDD_WALLET_VER );
+	wp_enqueue_style( 'edd-wallet', EDD_WALLET_URL . 'assets/css/edd-wallet' . $suffix . '.css', EDD_WALLET_VER );
 
-    if( edd_get_option( 'edd_wallet_disable_styles', false ) != true ) {
-        wp_enqueue_style( 'edd-wallet-deposit', EDD_WALLET_URL . 'assets/css/deposit' . $suffix . '.css', EDD_WALLET_VER );
-    }
+	if( edd_get_option( 'edd_wallet_disable_styles', false ) != true ) {
+		wp_enqueue_style( 'edd-wallet-deposit', EDD_WALLET_URL . 'assets/css/deposit' . $suffix . '.css', EDD_WALLET_VER );
+	}
 
-    $fee = EDD()->fees->get_fee( 'edd-wallet-deposit' );
+	$fee = EDD()->fees->get_fee( 'edd-wallet-deposit' );
 
-    if( $fee ) {
-        wp_enqueue_script( 'edd-wallet', EDD_WALLET_URL . 'assets/js/edd-wallet' . $suffix . '.js', EDD_WALLET_VER );
-    }
+	if( $fee ) {
+		wp_enqueue_script( 'edd-wallet', EDD_WALLET_URL . 'assets/js/edd-wallet' . $suffix . '.js', EDD_WALLET_VER );
+	}
 }
 add_action( 'wp_enqueue_scripts', 'edd_wallet_scripts' );
