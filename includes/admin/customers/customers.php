f904ebcb2c5251d0a69d7a4632e0987630e17bfb
Ghost1227 <dgriffiths@section214.com>
2015-09-22 19:22:39 -0500

    Fix whitespace

--- includes/admin/customers/customers.php
+++ includes/admin/customers/customers.php
@@ -9,7 +9,7 @@
 
 // Exit if accessed directly
 if( ! defined( 'ABSPATH' ) ) {
-    exit;
+	exit;
 }
 
 
@@ -21,9 +21,9 @@ if( ! defined( 'ABSPATH' ) ) {
  * @return      array $views The updated views
  */
 function edd_wallet_customer_view( $views ) {
-    $views['wallet'] = 'edd_customer_wallet_view';
+	$views['wallet'] = 'edd_customer_wallet_view';
 
-    return $views;
+	return $views;
 }
 add_filter( 'edd_customer_views', 'edd_wallet_customer_view' );
 
@@ -36,12 +36,12 @@ add_filter( 'edd_customer_views', 'edd_wallet_customer_view' );
  * @return      array $tabs The updated tabs
  */
 function edd_wallet_customer_tabs( $tabs ) {
-    $tabs['wallet'] = array(
-        'dashicon'  => 'dashicons-money',
-        'title'     => __( 'Customer Wallet', 'edd-wallet' )
-    );
+	$tabs['wallet'] = array(
+		'dashicon'  => 'dashicons-money',
+		'title'     => __( 'Customer Wallet', 'edd-wallet' )
+	);
 
-    return $tabs;
+	return $tabs;
 }
 add_filter( 'edd_customer_tabs', 'edd_wallet_customer_tabs' );
 
@@ -54,119 +54,119 @@ add_filter( 'edd_customer_tabs', 'edd_wallet_customer_tabs' );
  * @return      void
  */
 function edd_customer_wallet_view( $customer ) {
-    settings_errors( 'edd-notices' );
-    ?>
-    <div id="wallet-header-wrapper" class="customer-section">
-        <?php echo get_avatar( $customer->email, 30 ); ?> <span><?php echo sprintf( __( '%s - Wallet', 'edd-wallet' ), $customer->name ); ?></span>
-    </div>
+	settings_errors( 'edd-notices' );
+	?>
+	<div id="wallet-header-wrapper" class="customer-section">
+		<?php echo get_avatar( $customer->email, 30 ); ?> <span><?php echo sprintf( __( '%s - Wallet', 'edd-wallet' ), $customer->name ); ?></span>
+	</div>
 
-    <?php do_action( 'edd_customer_wallet_before_stats', $customer ); ?>
-    <?php $value = get_user_meta( $customer->id, '_edd_wallet_value', true ); ?>
+	<?php do_action( 'edd_customer_wallet_before_stats', $customer ); ?>
+	<?php $value = get_user_meta( $customer->id, '_edd_wallet_value', true ); ?>
 
-    <div id="wallet-stats-wrapper" class="customer-section">
-        <ul>
-            <li>
-                <span class="dashicons dashicons-money"></span>&nbsp;<?php echo sprintf( __( '%s Available', 'edd-wallet' ), edd_currency_filter( edd_format_amount( $value ) ) ); ?>
-            </li>
-        </ul>
-    </div>
-    
-    <?php do_action( 'edd_customer_wallet_before_tables_wrapper', $customer ); ?>
-    
-    <div id="wallet-tables-wrapper" class="customer-section">
+	<div id="wallet-stats-wrapper" class="customer-section">
+		<ul>
+			<li>
+				<span class="dashicons dashicons-money"></span>&nbsp;<?php echo sprintf( __( '%s Available', 'edd-wallet' ), edd_currency_filter( edd_format_amount( $value ) ) ); ?>
+			</li>
+		</ul>
+	</div>
 
-        <?php do_action( 'edd_customer_wallet_before_tables', $customer ); ?>
+	<?php do_action( 'edd_customer_wallet_before_tables_wrapper', $customer ); ?>
 
-        <h3><?php _e( 'Recent Activity', 'edd-wallet' ); ?></h3>
+	<div id="wallet-tables-wrapper" class="customer-section">
 
-        <?php
-        $activity = edd_wallet_get_activity( $customer->id );
-        ?>
-        
-        <table class="wp-list-table widefat striped activity">
-            <thead>
-                <tr>
-                    <th><?php _e( 'ID', 'edd-wallet' ); ?></th>
-                    <th><?php _e( 'Type', 'edd-wallet' ); ?></th>
-                    <th><?php _e( 'Amount', 'edd-wallet' ); ?></th>
-                    <th><?php _e( 'Date', 'edd-wallet' ); ?></th>
-                    <th><?php _e( 'Status', 'edd-wallet' ); ?></th>
-                    <th><?php _e( 'Actions', 'edd-wallet' ); ?></th>
-                </tr>
-            </thead>
-            <tbody>
+		<?php do_action( 'edd_customer_wallet_before_tables', $customer ); ?>
 
-            <?php
-            if( $activity ) {
-                foreach( $activity as $item ) {
-                    // Setup item type
-                    switch( $item->type ) {
-                        case 'deposit':
-                            $type = __( 'Deposit', 'edd-wallet' );
-                            $item_id = $item->id . ' (' . $item->payment_id . ')';
-                            $actions = '<a title="' . __( 'View Details for Payment', 'edd-wallet' ) . ' ' . $item->payment_id . '" href="' . admin_url( 'edit.php?post_type=download&page=edd-payment-history&view=view-order-details&id=' . $item->payment_id ) . '">' . __( 'View Details', 'edd-wallet' ) . '</a>';
-                            $status = edd_get_payment_status( get_post( $item->payment_id ), true );
-                            break;
-                        case 'withdrawal':
-                            $type = __( 'Withdraw', 'edd-wallet' );
-                            $item_id = $item->id . ' (' . $item->payment_id . ')';
-                            $actions = '<a title="' . __( 'View Details for Payment', 'edd-wallet' ) . ' ' . $item->payment_id . '" href="' . admin_url( 'edit.php?post_type=download&page=edd-payment-history&view=view-order-details&id=' . $item->payment_id ) . '">' . __( 'View Details', 'edd-wallet' ) . '</a>';
-                            $status = edd_get_payment_status( get_post( $item->payment_id ), true );
-                            break;
-                        case 'admin-deposit':
-                            $type = __( 'Admin Deposit', 'edd-wallet' );
-                            $item_id = $item->id;
-                            $actions = '';
-                            $status = __( 'Complete', 'edd-wallet' );
-                            break;
-                        case 'admin-withdraw':
-                            $type = __( 'Admin Withdraw', 'edd-wallet' );
-                            $item_id = $item->id;
-                            $actions = '';
-                            $status = __( 'Complete', 'edd-wallet' );
-                            break;
-                        case 'refund':
-                            $type = __( 'Refund', 'edd-wallet' );
-                            $item_id = $item->id;
-                            $actions = '';
-                            $status = __( 'Complete', 'edd-wallet' );
-                            break;
-                        default:
-                            $type = apply_filters( 'edd_wallet_activity_type', $item->type, $item );
-                            $item_id = apply_filters( 'edd_wallet_activity_item_id', $item->id, $item );
-                            $actions = apply_filters( 'edd_wallet_activity_actions', '', $item );
-                            $status = apply_filters( 'edd_wallet_activity_status', __( 'Complete', 'edd-wallet' ), $item );
-                            break;
-                    }
-                    ?>
-                    <tr>
-                        <td><?php echo $item_id; ?></td>
-                        <td><?php echo $type; ?></td>
-                        <td><?php echo edd_currency_filter( edd_format_amount( $item->amount ) ); ?></td>
-                        <td><?php echo $item->date_created; ?></td>
-                        <td><?php echo $status; ?></td>
-                        <td>
-                            <?php echo $actions; ?>
-                            <?php do_action( 'edd_wallet_recent_history_actions', $customer, $item ); ?>
-                        </td>
-                    </tr>
-                    <?php
-                }
-            } else {
-                echo '<tr><td colspan="6">' . __( 'No Activity Found', 'edd-wallet' ) . '</td></tr>';
-            }
-            ?>
+		<h3><?php _e( 'Recent Activity', 'edd-wallet' ); ?></h3>
 
-            </tbody>
-        </table>
+		<?php
+		$activity = edd_wallet_get_activity( $customer->id );
+		?>
 
-        <div class="edd-wallet-edit-wallet">
-            <a class="button-secondary" href="<?php echo esc_url( add_query_arg( array( 'page' => 'edd-wallet-edit', 'id' => $customer->id, 'edd-message' => false ), admin_url( 'options.php' ) ) ); ?>"><?php _e( 'Edit Wallet', 'edd-wallet' ); ?></a>
-        </div>
+		<table class="wp-list-table widefat striped activity">
+			<thead>
+				<tr>
+					<th><?php _e( 'ID', 'edd-wallet' ); ?></th>
+					<th><?php _e( 'Type', 'edd-wallet' ); ?></th>
+					<th><?php _e( 'Amount', 'edd-wallet' ); ?></th>
+					<th><?php _e( 'Date', 'edd-wallet' ); ?></th>
+					<th><?php _e( 'Status', 'edd-wallet' ); ?></th>
+					<th><?php _e( 'Actions', 'edd-wallet' ); ?></th>
+				</tr>
+			</thead>
+			<tbody>
 
-        <?php do_action( 'edd_customer_wallet_after_tables', $customer ); ?>
+			<?php
+			if( $activity ) {
+				foreach( $activity as $item ) {
+					// Setup item type
+					switch( $item->type ) {
+						case 'deposit':
+							$type = __( 'Deposit', 'edd-wallet' );
+							$item_id = $item->id . ' (' . $item->payment_id . ')';
+							$actions = '<a title="' . __( 'View Details for Payment', 'edd-wallet' ) . ' ' . $item->payment_id . '" href="' . admin_url( 'edit.php?post_type=download&page=edd-payment-history&view=view-order-details&id=' . $item->payment_id ) . '">' . __( 'View Details', 'edd-wallet' ) . '</a>';
+							$status = edd_get_payment_status( get_post( $item->payment_id ), true );
+							break;
+						case 'withdrawal':
+							$type = __( 'Withdraw', 'edd-wallet' );
+							$item_id = $item->id . ' (' . $item->payment_id . ')';
+							$actions = '<a title="' . __( 'View Details for Payment', 'edd-wallet' ) . ' ' . $item->payment_id . '" href="' . admin_url( 'edit.php?post_type=download&page=edd-payment-history&view=view-order-details&id=' . $item->payment_id ) . '">' . __( 'View Details', 'edd-wallet' ) . '</a>';
+							$status = edd_get_payment_status( get_post( $item->payment_id ), true );
+							break;
+						case 'admin-deposit':
+							$type = __( 'Admin Deposit', 'edd-wallet' );
+							$item_id = $item->id;
+							$actions = '';
+							$status = __( 'Complete', 'edd-wallet' );
+							break;
+						case 'admin-withdraw':
+							$type = __( 'Admin Withdraw', 'edd-wallet' );
+							$item_id = $item->id;
+							$actions = '';
+							$status = __( 'Complete', 'edd-wallet' );
+							break;
+						case 'refund':
+							$type = __( 'Refund', 'edd-wallet' );
+							$item_id = $item->id;
+							$actions = '';
+							$status = __( 'Complete', 'edd-wallet' );
+							break;
+						default:
+							$type = apply_filters( 'edd_wallet_activity_type', $item->type, $item );
+							$item_id = apply_filters( 'edd_wallet_activity_item_id', $item->id, $item );
+							$actions = apply_filters( 'edd_wallet_activity_actions', '', $item );
+							$status = apply_filters( 'edd_wallet_activity_status', __( 'Complete', 'edd-wallet' ), $item );
+							break;
+					}
+					?>
+					<tr>
+						<td><?php echo $item_id; ?></td>
+						<td><?php echo $type; ?></td>
+						<td><?php echo edd_currency_filter( edd_format_amount( $item->amount ) ); ?></td>
+						<td><?php echo $item->date_created; ?></td>
+						<td><?php echo $status; ?></td>
+						<td>
+							<?php echo $actions; ?>
+							<?php do_action( 'edd_wallet_recent_history_actions', $customer, $item ); ?>
+						</td>
+					</tr>
+					<?php
+				}
+			} else {
+				echo '<tr><td colspan="6">' . __( 'No Activity Found', 'edd-wallet' ) . '</td></tr>';
+			}
+			?>
 
-    </div>
-    <?php
-    do_action( 'edd_customer_wallet_card_bottom', $customer );
+			</tbody>
+		</table>
+
+		<div class="edd-wallet-edit-wallet">
+			<a class="button-secondary" href="<?php echo esc_url( add_query_arg( array( 'page' => 'edd-wallet-edit', 'id' => $customer->id, 'edd-message' => false ), admin_url( 'options.php' ) ) ); ?>"><?php _e( 'Edit Wallet', 'edd-wallet' ); ?></a>
+		</div>
+
+		<?php do_action( 'edd_customer_wallet_after_tables', $customer ); ?>
+
+	</div>
+	<?php
+	do_action( 'edd_customer_wallet_card_bottom', $customer );
 }
